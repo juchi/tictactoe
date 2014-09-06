fcc550958b80dc393b5c769fd0d4ac3f0ed88ddb
Julien Chichignoud <julien.chichignoud@gmail.com>
2014-09-06 16:52:43 +0200

    improve http server for file read

--- server.js
+++ server.js
@@ -2,15 +2,28 @@ var fs = require('fs');
 
 function onRequest(request, response)
 {
-	if (request.url != '/') {
-		var res = fs.readFileSync('./front/'+request.url);
-		response.end(res);
-	} else {
-		var html = fs.readFileSync('front/index.html');
-		response.writeHead({'Content-Type': 'text/html'});
-		response.write(html);
-		response.end();
-	}
+    if (request.url != '/') {
+        fs.readFile('./front/'+request.url, function(err, data) {
+            if (err) {
+                response.writeHead(404);
+                response.end();
+            } else {
+                response.end(data);
+            }
+        });
+    } else {
+        fs.readFile('front/index.html', function (err, data) {
+            if (err) {
+                response.writeHead(404);
+                response.end();
+            } else {
+                response.writeHead({'Content-Type': 'text/html'});
+                response.write(data);
+                response.end();
+            }
+
+        });
+    }
 }
 
 exports.onRequest = onRequest;
