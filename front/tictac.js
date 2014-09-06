ee73ef41b9d0e56127946feb80adeadc63f24ff2
Julien Chichignoud <julien.chichignoud@gmail.com>
2014-09-06 17:12:07 +0200

    remove tab indents

--- front/tictac.js
+++ front/tictac.js
@@ -1,130 +1,130 @@
 var tictactoe = {
-	ctx: null,
-	cellSize: 50,
-	playerColors: ['blue', 'red'],
-	grid: [[0, 0, 0], [0, 0, 0], [0, 0, 0]],
-	players: [{shape:'cross', color:'blue'}, {shape:'circle', color:'red'}],
-	
-
-	init: function(canvas)
-	{
-		this.ctx = canvas.getContext('2d');
-		canvas.width = this.cellSize * 3;
-		canvas.height = this.cellSize * 3;
-	
-		this.drawGrid();
-	},
-	
-	drawGrid: function()
-	{
-		var ctx = this.ctx;
-		ctx.strokeStyle = 'black';
-		ctx.beginPath();
-		ctx.moveTo(this.cellSize, 0);
-		ctx.lineTo(this.cellSize, this.cellSize * 3);
-		ctx.stroke();
-		ctx.moveTo(this.cellSize * 2, 0);
-		ctx.lineTo(this.cellSize * 2, this.cellSize * 3);
-		ctx.stroke();
-	
-		ctx.moveTo(0, this.cellSize);
-		ctx.lineTo(this.cellSize * 3, this.cellSize);
-		ctx.stroke();
-		ctx.moveTo(0, this.cellSize * 2);
-		ctx.lineTo(this.cellSize * 3, this.cellSize * 2);
-		ctx.stroke();
-	
-	},
+    ctx: null,
+    cellSize: 50,
+    playerColors: ['blue', 'red'],
+    grid: [[0, 0, 0], [0, 0, 0], [0, 0, 0]],
+    players: [{shape:'cross', color:'blue'}, {shape:'circle', color:'red'}],
 
 
-	getCellFromPosition: function(pos) {
-		var coords = {};
-		coords.x = Math.floor(pos.x / this.cellSize);
-		coords.y = Math.floor(pos.y / this.cellSize);
-		return coords;
-	},
+    init: function(canvas)
+    {
+        this.ctx = canvas.getContext('2d');
+        canvas.width = this.cellSize * 3;
+        canvas.height = this.cellSize * 3;
 
-	getTopLeftPosition: function(coords) {
-		var pos = {};
-		pos.x = coords.x * this.cellSize;
-		pos.y = coords.y * this.cellSize;
-		return pos;
-	},
-	
-	drawPiece: function(coords, playerIndex) {
-		drawFunction = this.drawFunctions[players[playerIndex].shape];
-		drawFunction(this.getTopLeftPosition(coords), players[playerIndex].color);
-	},
+        this.drawGrid();
+    },
 
-	
-	drawFunctions: {
-		'cross': function(position, color) {
-			var cellSize = tictactoe.cellSize;
-			var ctx = tictactoe.ctx;
-			ctx.strokeStyle = color;
-			ctx.beginPath();
-			ctx.moveTo(position.x + cellSize / 10, position.y + cellSize / 10);
-			ctx.lineTo(position.x + cellSize - cellSize / 10,
-					position.y + cellSize - cellSize / 10);
-			ctx.moveTo(position.x + cellSize - cellSize / 10, position.y + cellSize / 10);
-			ctx.lineTo(position.x + cellSize / 10, position.y + cellSize - cellSize / 10);
-			ctx.stroke();
-		},
-		'circle': function(position, color) {
-			var cellSize = tictactoe.cellSize;
-			var ctx = tictactoe.ctx;
-			ctx.strokeStyle = color;
-			ctx.beginPath();
-			ctx.arc(position.x + cellSize / 2,
-					position.y + cellSize / 2,
-					2 * cellSize / 5,
-					0,
-					Math.PI * 2);
-			ctx.stroke();
-		}
-	},
+    drawGrid: function()
+    {
+        var ctx = this.ctx;
+        ctx.strokeStyle = 'black';
+        ctx.beginPath();
+        ctx.moveTo(this.cellSize, 0);
+        ctx.lineTo(this.cellSize, this.cellSize * 3);
+        ctx.stroke();
+        ctx.moveTo(this.cellSize * 2, 0);
+        ctx.lineTo(this.cellSize * 2, this.cellSize * 3);
+        ctx.stroke();
 
-	actions: {
-		connection: function(message) {
-			playerIndex = message.index;
-			players = message.players;
-		},
-		newplayer: function(message) {
-			players = message.players;
-			$('#game-info').text('A new player just connected');
-		},
-		newgame: function(message) {
-			var info = 'The game starts now<br/>';
-			if (message.next == playerIndex) {
-				info += 'Your turn to play';
-			} else {
-				info += 'Waiting for player ' + message.next;
-			}
-			$('#game-info').html(info);
-		},
-		move: function(message) {
-			tictactoe.drawPiece(message.coords, message.index);
-		
-			var winner = message.win;
-			var info = '';
-			if (winner != -1) {
-				if (winner == playerIndex) {
-					info = 'You won !';
-				} else {
-					info = 'Player '+winner+' won !';
-				}
-			} else {
-				if (message.next == playerIndex) {
-					info = 'Your turn to play';
-				} else {
-					info = 'Waiting for player ' + message.next;
-				}
-			}
-			$('#game-info').text(info);
-		},
-		message: function(message) {
-			$('#game-info').text(message.text);
-		}
-	}
+        ctx.moveTo(0, this.cellSize);
+        ctx.lineTo(this.cellSize * 3, this.cellSize);
+        ctx.stroke();
+        ctx.moveTo(0, this.cellSize * 2);
+        ctx.lineTo(this.cellSize * 3, this.cellSize * 2);
+        ctx.stroke();
+
+    },
+
+
+    getCellFromPosition: function(pos) {
+        var coords = {};
+        coords.x = Math.floor(pos.x / this.cellSize);
+        coords.y = Math.floor(pos.y / this.cellSize);
+        return coords;
+    },
+
+    getTopLeftPosition: function(coords) {
+        var pos = {};
+        pos.x = coords.x * this.cellSize;
+        pos.y = coords.y * this.cellSize;
+        return pos;
+    },
+
+    drawPiece: function(coords, playerIndex) {
+        drawFunction = this.drawFunctions[players[playerIndex].shape];
+        drawFunction(this.getTopLeftPosition(coords), players[playerIndex].color);
+    },
+
+
+    drawFunctions: {
+        'cross': function(position, color) {
+            var cellSize = tictactoe.cellSize;
+            var ctx = tictactoe.ctx;
+            ctx.strokeStyle = color;
+            ctx.beginPath();
+            ctx.moveTo(position.x + cellSize / 10, position.y + cellSize / 10);
+            ctx.lineTo(position.x + cellSize - cellSize / 10,
+                    position.y + cellSize - cellSize / 10);
+            ctx.moveTo(position.x + cellSize - cellSize / 10, position.y + cellSize / 10);
+            ctx.lineTo(position.x + cellSize / 10, position.y + cellSize - cellSize / 10);
+            ctx.stroke();
+        },
+        'circle': function(position, color) {
+            var cellSize = tictactoe.cellSize;
+            var ctx = tictactoe.ctx;
+            ctx.strokeStyle = color;
+            ctx.beginPath();
+            ctx.arc(position.x + cellSize / 2,
+                    position.y + cellSize / 2,
+                    2 * cellSize / 5,
+                    0,
+                    Math.PI * 2);
+            ctx.stroke();
+        }
+    },
+
+    actions: {
+        connection: function(message) {
+            playerIndex = message.index;
+            players = message.players;
+        },
+        newplayer: function(message) {
+            players = message.players;
+            $('#game-info').text('A new player just connected');
+        },
+        newgame: function(message) {
+            var info = 'The game starts now<br/>';
+            if (message.next == playerIndex) {
+                info += 'Your turn to play';
+            } else {
+                info += 'Waiting for player ' + message.next;
+            }
+            $('#game-info').html(info);
+        },
+        move: function(message) {
+            tictactoe.drawPiece(message.coords, message.index);
+
+            var winner = message.win;
+            var info = '';
+            if (winner != -1) {
+                if (winner == playerIndex) {
+                    info = 'You won !';
+                } else {
+                    info = 'Player '+winner+' won !';
+                }
+            } else {
+                if (message.next == playerIndex) {
+                    info = 'Your turn to play';
+                } else {
+                    info = 'Waiting for player ' + message.next;
+                }
+            }
+            $('#game-info').text(info);
+        },
+        message: function(message) {
+            $('#game-info').text(message.text);
+        }
+    }
 };
 
--- tictac.js
+++ tictac.js
@@ -1,146 +1,146 @@
 var tictactoe = function() {
 
-	var games = [];
+    var games = [];
 
-	var Player = function(connection) {
-		connection.player = this;
+    var Player = function(connection) {
+        connection.player = this;
 
-		this.index = 0;
-		this.game = null;
+        this.index = 0;
+        this.game = null;
 
-		this.sendMessage = function(message) {
-			connection.sendText(message);
-		};
-	};
+        this.sendMessage = function(message) {
+            connection.sendText(message);
+        };
+    };
 
-	var Game = function() {
-		var indexes = [0, 1];
-		var colors = ['blue', 'red'];
-		var shapes = ['cross', 'circle'];
-		var grid = [];
-		var nextTurn = -1;
-		var running = false;
+    var Game = function() {
+        var indexes = [0, 1];
+        var colors = ['blue', 'red'];
+        var shapes = ['cross', 'circle'];
+        var grid = [];
+        var nextTurn = -1;
+        var running = false;
 
-		var players = [];
-		var playersData = [];
+        var players = [];
+        var playersData = [];
 
-		this.newPlayer = function() {
-			if (indexes.length == 0) return;
-			var index = indexes.shift();
+        this.newPlayer = function() {
+            if (indexes.length == 0) return;
+            var index = indexes.shift();
 
-			player.index = index;
-			player.game = this;
-			players[index] = player;
-			playersData[index] = {'color':colors.shift(), 'shape':shapes.shift()};
+            player.index = index;
+            player.game = this;
+            players[index] = player;
+            playersData[index] = {'color':colors.shift(), 'shape':shapes.shift()};
 
-			var initMessage = {'type':'connection','index':index, 'players':playersData};
-			player.sendMessage(JSON.stringify(initMessage));
-			broadcast(players, JSON.stringify({'type':'newplayer', 'players':playersData}), index);
+            var initMessage = {'type':'connection','index':index, 'players':playersData};
+            player.sendMessage(JSON.stringify(initMessage));
+            broadcast(players, JSON.stringify({'type':'newplayer', 'players':playersData}), index);
 
-			if (indexes.length == 0) {
-				this.start();
-			} else {
-				broadcast(players, JSON.stringify({'type':'message', 'text':'Waiting for another player...'}));
-			}
-		};
+            if (indexes.length == 0) {
+                this.start();
+            } else {
+                broadcast(players, JSON.stringify({'type':'message', 'text':'Waiting for another player...'}));
+            }
+        };
 
-		this.start = function() {
-			running = true;
-			nextTurn = 0;
-			grid = [[-1, -1, -1], [-1, -1, -1], [-1, -1,-1]];
+        this.start = function() {
+            running = true;
+            nextTurn = 0;
+            grid = [[-1, -1, -1], [-1, -1, -1], [-1, -1,-1]];
 
-			var message = {'type':'newgame', 'next':nextTurn};
-			message = JSON.stringify(message);
-			broadcast(players, message);
-		};
+            var message = {'type':'newgame', 'next':nextTurn};
+            message = JSON.stringify(message);
+            broadcast(players, message);
+        };
 
-		this.hasFreeSlot = function() {
-			return indexes.length > 0;
-		};
+        this.hasFreeSlot = function() {
+            return indexes.length > 0;
+        };
 
-		this.processMove = function (coords, player)
-		{console.log('processmove');
-			if (!running) {
-				var message = {'type':'message','text':'The game did not begin yet.'};
-				message = JSON.stringify(message);
-				player.sendMessage(message);
-				return;
-			}
-			if (nextTurn != player.index) {
-				var message = {'type':'message','text':'Its not your turn to play.'};
-				message = JSON.stringify(message);
-				player.sendMessage(message);
-				return;
-			}
+        this.processMove = function (coords, player)
+        {console.log('processmove');
+            if (!running) {
+                var message = {'type':'message','text':'The game did not begin yet.'};
+                message = JSON.stringify(message);
+                player.sendMessage(message);
+                return;
+            }
+            if (nextTurn != player.index) {
+                var message = {'type':'message','text':'Its not your turn to play.'};
+                message = JSON.stringify(message);
+                player.sendMessage(message);
+                return;
+            }
 
-			if (grid[coords.x][coords.y] == -1) {
-				grid[coords.x][coords.y] = player.index;
-				nextTurn++;
-				nextTurn %= 2;
+            if (grid[coords.x][coords.y] == -1) {
+                grid[coords.x][coords.y] = player.index;
+                nextTurn++;
+                nextTurn %= 2;
 
-				var win = checkWin(grid);
-				var message = {
-					'type':'move',
-					'coords':coords,
-					'index':player.index,
-					'next':nextTurn,
-					'win':win
-				};
-				message = JSON.stringify(message);
-				broadcast(players, message);
+                var win = checkWin(grid);
+                var message = {
+                    'type':'move',
+                    'coords':coords,
+                    'index':player.index,
+                    'next':nextTurn,
+                    'win':win
+                };
+                message = JSON.stringify(message);
+                broadcast(players, message);
 
-				if (win != -1) {
-					running = false;
-				}
+                if (win != -1) {
+                    running = false;
+                }
 
-			} else {
-				var message = {'type':'message','text':'Move is not allowed'};
-				message = JSON.stringify(message);
-				player.sendMessage(message);
-			}
-		};
+            } else {
+                var message = {'type':'message','text':'Move is not allowed'};
+                message = JSON.stringify(message);
+                player.sendMessage(message);
+            }
+        };
 
-		this.onPlayerQuit = function(player) {
-			index = player.index;
-			player.game = null;
+        this.onPlayerQuit = function(player) {
+            index = player.index;
+            player.game = null;
 
-			colors.push(playersData[index].color);
-			shapes.push(playersData[index].shape);
+            colors.push(playersData[index].color);
+            shapes.push(playersData[index].shape);
 
-			players[index] = null;
-			playersData[index] = null;
+            players[index] = null;
+            playersData[index] = null;
 
-			indexes.push(index);
-			nextTurn = -1;
-			running = false;
-		};
-	};
+            indexes.push(index);
+            nextTurn = -1;
+            running = false;
+        };
+    };
 
-	function getGameInstance() {
-		for (var i = 0; i < games.length; i++) {
-			if (games[i].hasFreeSlot()) {
-				return games[i];
-			}
-		}
+    function getGameInstance() {
+        for (var i = 0; i < games.length; i++) {
+            if (games[i].hasFreeSlot()) {
+                return games[i];
+            }
+        }
 
-		var game = new Game();
-		games.push(game);
-		return game;
-	}
+        var game = new Game();
+        games.push(game);
+        return game;
+    }
 
-	this.initConnection = function(connection) {
-		console.log('client connection');
+    this.initConnection = function(connection) {
+        console.log('client connection');
 
-		player = new Player(connection);
+        player = new Player(connection);
 
-		var game = getGameInstance();
-		game.newPlayer(player);
+        var game = getGameInstance();
+        game.newPlayer(player);
 
-		connection.on('text', manageMessage);
-		connection.on('close', manageClose);
-	};
+        connection.on('text', manageMessage);
+        connection.on('close', manageClose);
+    };
 
-	function manageMessage(message) {
+    function manageMessage(message) {
         console.log(message);
         message = JSON.parse(message);
         switch (message.type) {
@@ -151,97 +151,97 @@ var tictactoe = function() {
             default:
                 break;
         }
-	}
+    }
 
-	function manageClose() {
-		console.log('connection close');
+    function manageClose() {
+        console.log('connection close');
 
-		this.player.game.onPlayerQuit(this.player);
-	}
+        this.player.game.onPlayerQuit(this.player);
+    }
 
-	function checkWin(grid)
-	{
-		var current = -1;
-		// check columns
-		for (var i = 0; i < grid.length; i++) {
-			for (var j = 0; j < grid.length; j++) {
-				if (grid[i][j] == -1) {
-					current = -1;
-					break;
-				}
-				if (grid[i][j] == current || current == -1) {
-					current = grid[i][j];
-					if (j == grid.length - 1) {
-						return current;
-					}
-				} else {
-					current = -1;
-					break;
-				}
-			}
-		}
+    function checkWin(grid)
+    {
+        var current = -1;
+        // check columns
+        for (var i = 0; i < grid.length; i++) {
+            for (var j = 0; j < grid.length; j++) {
+                if (grid[i][j] == -1) {
+                    current = -1;
+                    break;
+                }
+                if (grid[i][j] == current || current == -1) {
+                    current = grid[i][j];
+                    if (j == grid.length - 1) {
+                        return current;
+                    }
+                } else {
+                    current = -1;
+                    break;
+                }
+            }
+        }
 
-		// check lines
-		for (var i = 0; i < grid.length; i++) {
-			for (var j = 0; j < grid.length; j++) {
-				if (grid[j][i] == -1) {
-					current = -1;
-					break;
-				}
-				if (grid[j][i] == current || current == -1) {
-					current = grid[j][i];
-					if (j == grid.length - 1) {
-						return current;
-					}
-				} else {
-					current = -1;
-					break;
-				}
-			}
-		}
+        // check lines
+        for (var i = 0; i < grid.length; i++) {
+            for (var j = 0; j < grid.length; j++) {
+                if (grid[j][i] == -1) {
+                    current = -1;
+                    break;
+                }
+                if (grid[j][i] == current || current == -1) {
+                    current = grid[j][i];
+                    if (j == grid.length - 1) {
+                        return current;
+                    }
+                } else {
+                    current = -1;
+                    break;
+                }
+            }
+        }
 
-		// check diags
-		for (var i = 0; i < grid.length; i++) {
-			if (grid[i][i] == -1) {
-				current = -1;
-				break;
-			}
-			if (grid[i][i] == current || current == -1) {
-				current = grid[i][i];
-				if (i == grid.length - 1) {
-					return current;
-				}
-			} else {
-				current = -1;
-				break;
-			}
-		}
-		for (var i = 0; i < grid.length; i++) {
-			var j = grid.length - i - 1;
-			if (grid[i][j] == -1) {
-				break;
-			}
-			if (grid[i][j] == current || current == -1) {
-				current = grid[i][j];
-				if (i == grid.length - 1) {
-					return current;
-				}
-			} else {
-				current = -1;
-				break;
-			}
-		}
+        // check diags
+        for (var i = 0; i < grid.length; i++) {
+            if (grid[i][i] == -1) {
+                current = -1;
+                break;
+            }
+            if (grid[i][i] == current || current == -1) {
+                current = grid[i][i];
+                if (i == grid.length - 1) {
+                    return current;
+                }
+            } else {
+                current = -1;
+                break;
+            }
+        }
+        for (var i = 0; i < grid.length; i++) {
+            var j = grid.length - i - 1;
+            if (grid[i][j] == -1) {
+                break;
+            }
+            if (grid[i][j] == current || current == -1) {
+                current = grid[i][j];
+                if (i == grid.length - 1) {
+                    return current;
+                }
+            } else {
+                current = -1;
+                break;
+            }
+        }
 
-		return -1;
-	}
+        return -1;
+    }
 
-	function broadcast(players, message, exceptIndex)
-	{
-		for (var i = 0; i < players.length; i++) {
-			if (i != exceptIndex && players[i] != null)
-				players[i].sendMessage(message);
-		}
-	}
+    function broadcast(players, message, exceptIndex)
+    {
+        for (var i = 0; i < players.length; i++) {
+            if (i != exceptIndex && players[i] != null)
+                players[i].sendMessage(message);
+        }
+    }
 }
 
 exports.tictactoe = tictactoe;
