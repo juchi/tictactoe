cbb554784527228ec9a71605c061762154690825
Julien Chichignoud <julien.chichignoud@gmail.com>
2014-09-06 17:50:36 +0200

    refactor the object structure on frontend script

--- front/tictac.js
+++ front/tictac.js
@@ -1,15 +1,17 @@
 "use strict";
 
-var tictactoe = {
+(function() {
+var tictactoe = function() {
+
+};
+tictactoe.prototype = {
     ctx: null,
     cellSize: 50,
     playerColors: ['blue', 'red'],
     grid: [[0, 0, 0], [0, 0, 0], [0, 0, 0]],
     players: [{shape:'cross', color:'blue'}, {shape:'circle', color:'red'}],
 
-
-    init: function(canvas)
-    {
+    init: function(canvas) {
         this.ctx = canvas.getContext('2d');
         canvas.width = this.cellSize * 3;
         canvas.height = this.cellSize * 3;
@@ -17,8 +19,7 @@ var tictactoe = {
         this.drawGrid();
     },
 
-    drawGrid: function()
-    {
+    drawGrid: function() {
         var ctx = this.ctx;
         ctx.strokeStyle = 'black';
         ctx.beginPath();
@@ -38,7 +39,6 @@ var tictactoe = {
 
     },
 
-
     getCellFromPosition: function(pos) {
         var coords = {};
         coords.x = Math.floor(pos.x / this.cellSize);
@@ -54,79 +54,91 @@ var tictactoe = {
     },
 
     drawPiece: function(coords, playerIndex) {
-        var drawFunction = this.drawFunctions[tictactoe.players[playerIndex].shape];
-        drawFunction(this.getTopLeftPosition(coords), tictactoe.players[playerIndex].color);
+        var shape = this.players[playerIndex].shape;
+        var position = this.getTopLeftPosition(coords);
+        var color = this.players[playerIndex].color;
+        this.draw(shape, position, color);
     },
 
-
-    drawFunctions: {
-        'cross': function(position, color) {
-            var cellSize = tictactoe.cellSize;
-            var ctx = tictactoe.ctx;
-            ctx.strokeStyle = color;
-            ctx.beginPath();
-            ctx.moveTo(position.x + cellSize / 10, position.y + cellSize / 10);
-            ctx.lineTo(position.x + cellSize - cellSize / 10,
-                    position.y + cellSize - cellSize / 10);
-            ctx.moveTo(position.x + cellSize - cellSize / 10, position.y + cellSize / 10);
-            ctx.lineTo(position.x + cellSize / 10, position.y + cellSize - cellSize / 10);
-            ctx.stroke();
-        },
-        'circle': function(position, color) {
-            var cellSize = tictactoe.cellSize;
-            var ctx = tictactoe.ctx;
-            ctx.strokeStyle = color;
-            ctx.beginPath();
-            ctx.arc(position.x + cellSize / 2,
-                    position.y + cellSize / 2,
-                    2 * cellSize / 5,
+    draw: function(shape, position, color) {
+        var that = this;
+        var shapes = {
+            'cross': function (position, color) {
+                var cellSize = that.cellSize;
+                var ctx = that.ctx;
+                ctx.strokeStyle = color;
+                ctx.beginPath();
+                ctx.moveTo(position.x + cellSize / 10, position.y + cellSize / 10);
+                ctx.lineTo(position.x + cellSize - cellSize / 10,
+                        position.y + cellSize - cellSize / 10);
+                ctx.moveTo(position.x + cellSize - cellSize / 10, position.y + cellSize / 10);
+                ctx.lineTo(position.x + cellSize / 10, position.y + cellSize - cellSize / 10);
+                ctx.stroke();
+            },
+            'circle': function (position, color) {
+                var cellSize = that.cellSize;
+                var ctx = that.ctx;
+                ctx.strokeStyle = color;
+                ctx.beginPath();
+                ctx.arc(position.x + cellSize / 2,
+                        position.y + cellSize / 2,
+                        2 * cellSize / 5,
                     0,
-                    Math.PI * 2);
-            ctx.stroke();
-        }
+                        Math.PI * 2);
+                ctx.stroke();
+            }
+        };
+        shapes[shape](position, color);
     },
 
-    actions: {
-        connection: function(message) {
-            playerIndex = message.index;
-            tictactoe.players = message.players;
-        },
-        newplayer: function(message) {
-            tictactoe.players = message.players;
-            $('#game-info').text('A new player just connected');
-        },
-        newgame: function(message) {
-            var info = 'The game starts now<br/>';
-            if (message.next == playerIndex) {
-                info += 'Your turn to play';
-            } else {
-                info += 'Waiting for player ' + message.next;
-            }
-            $('#game-info').html(info);
-        },
-        move: function(message) {
-            tictactoe.drawPiece(message.coords, message.index);
-
-            var winner = message.win;
-            var info = '';
-            if (winner != -1) {
-                if (winner == playerIndex) {
-                    info = 'You won !';
-                } else {
-                    info = 'Player '+winner+' won !';
-                }
-            } else {
+    runAction: function(action, message) {
+        var that = this;
+        var actions = {
+            connection: function (message) {
+                playerIndex = message.index;
+                that.players = message.players;
+            },
+            newplayer: function (message) {
+                that.players = message.players;
+                $('#game-info').text('A new player just connected');
+            },
+            newgame: function (message) {
+                var info = 'The game starts now<br/>';
                 if (message.next == playerIndex) {
-                    info = 'Your turn to play';
+                    info += 'Your turn to play';
                 } else {
-                    info = 'Waiting for player ' + message.next;
+                    info += 'Waiting for player ' + message.next;
                 }
+                $('#game-info').html(info);
+            },
+            move: function (message) {
+                that.drawPiece(message.coords, message.index);
+
+                var winner = message.win;
+                var info = '';
+                if (winner != -1) {
+                    if (winner == playerIndex) {
+                        info = 'You won !';
+                    } else {
+                        info = 'Player ' + winner + ' won !';
+                    }
+                } else {
+                    if (message.next == playerIndex) {
+                        info = 'Your turn to play';
+                    } else {
+                        info = 'Waiting for player ' + message.next;
+                    }
+                }
+                $('#game-info').text(info);
+            },
+            message: function (message) {
+                $('#game-info').text(message.text);
             }
-            $('#game-info').text(info);
-        },
-        message: function(message) {
-            $('#game-info').text(message.text);
-        }
+        };
+        actions[action](message);
     }
 };
+window.Tictactoe = tictactoe;
+})();
+
 
